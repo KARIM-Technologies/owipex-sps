cffcc1565b29d28c695c07217427d0e395ddf48d
KarimSayedAmir <s.karim@wer.ag>
2023-11-20 22:24:31 +0100

    init

---
+++ flask_app/updated_app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, jsonify
+import subprocess
+
+app = Flask(__name__)
+process = None
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/start_program', methods=['POST'])
+def start_program():
+    global process
+    if process:
+        return "Programm bereits gestartet."
+
+    try:
+        process = subprocess.Popen(["python3", "/home/owipex/h2o.py"])
+        return "Programm gestartet."
+    except Exception as e:
+        return str(e)
+
+@app.route('/stop_program', methods=['POST'])
+def stop_program():
+    global process
+    if not process:
+        return "Programm nicht gestartet."
+    try:
+        process.terminate()
+        process = None
+        return "Programm gestoppt."
+    except Exception as e:
+        return str(e)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081, threaded=True)
+
+
+@app.route('/stop_program', methods=['POST'])
+def stop_program():
+    global process
+    if process:
+        process.terminate()  # Sends a SIGTERM signal
+        process = None
+        return "Programm gestoppt."
+    return "Kein laufendes Programm gefunden."
