9f52c48a1dcd2b6fb59db27b118d49299b0496c8
Sayed Amir Karim <s.karim@wer.ag>
2024-02-04 15:01:06 +0100

    added FlowCalibration Script for UP Board

--- tools/modbus/read/example/modbusExample.py
+++ tools/modbus/read/example/modbusExample.py
@@ -5,7 +5,7 @@
 #
 # License: All Rights Reserved
 #
-# Module: Test Modbus Lib V0.5
+# Module: Modbus Lib V0.6
 # Description: Modbus Communication Test Script
 # -----------------------------------------------------------------------------
 
@@ -15,7 +15,7 @@
 from modbus_lib import DeviceManager
 
 # Create DeviceManager
-dev_manager = DeviceManager(port='/dev/ttymxc3', baudrate=9600, parity='N', stopbits=1, bytesize=8, timeout=1)
+dev_manager = DeviceManager(port='/dev/ttymS0', baudrate=9600, parity='N', stopbits=1, bytesize=8, timeout=1)
 
 # Add devices
 dev_manager.add_device(device_id=0x01)
